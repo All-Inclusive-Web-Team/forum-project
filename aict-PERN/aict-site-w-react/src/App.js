db5b6d8e39873ff23c6572ebdf3f05ead5e78cfc
ddowy <92402470+ddowy@users.noreply.github.com>
2022-11-20 22:18:45 -0600

    Error from mergining fixed

--- aict-PERN/aict-site-w-react/src/App.js
+++ aict-PERN/aict-site-w-react/src/App.js
@@ -1,24 +1,27 @@
 import Navbar from "./components/Navbar/Navbar";
 import Main from "./components/Main/Main"
 import { useState } from "react";
+import { UserData } from "./UserData"
 
 function App() {
   const [logInRegisterPopUp, setLogInRegisterPopUp] = useState(false)
   const [userData, setUserData] = useState(null)
   return (
     <div>
-      <Navbar 
-      setLogInRegisterPopUp={setLogInRegisterPopUp} 
-      logInRegisterPopUp={logInRegisterPopUp}
-      userData={userData}
-      setUserData={setUserData}
-      />
-      <Main 
-      setLogInRegisterPopUp={setLogInRegisterPopUp}
-      logInRegisterPopUp={logInRegisterPopUp}
-      userData={userData}
-      setUserData={setUserData}
-      />
+      <UserData>
+        <Navbar 
+        setLogInRegisterPopUp={setLogInRegisterPopUp} 
+        logInRegisterPopUp={logInRegisterPopUp}
+        userData={userData}
+        setUserData={setUserData}
+        />
+        <Main 
+        setLogInRegisterPopUp={setLogInRegisterPopUp}
+        logInRegisterPopUp={logInRegisterPopUp}
+        userData={userData}
+        setUserData={setUserData}
+        />
+      </UserData>
     </div>
   );
 }
